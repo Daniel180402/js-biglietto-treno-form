70d322828f638c40507116475d4681082395a26c
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-02-23 17:21:28 +0100

    added random number generator

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ let basekm = 0.27;
 let price;
 
 document.querySelector("#btninput").addEventListener('click', function() {
+    document.getElementById("risultato").style.display = 'block';
     username = document.getElementById("namei");
     console.log(username.value);
     document.querySelector("#nome-pass").innerHTML = username.value;
@@ -18,19 +19,30 @@ document.querySelector("#btninput").addEventListener('click', function() {
     price = km * basekm;
     console.log(price.value);
 
+
     eta = document.getElementById("eta");
     console.log(eta.value);  
-    if(eta.value = "minorenne"){
+    if(eta = "minorenne"){
         discount = (price / 100) * 17;
         finalprice = price - discount;
         document.getElementById("tipo-biglietto").innerHTML = "Minori";
     }
-    else if(eta.value = "over65"){
+    else if(eta = "over65"){
         discount = (price / 100) * 33;
+        finalprice = price - discount;
         document.getElementById("tipo-biglietto").innerHTML = "Over 65";
     }
     else{
         finalprice = price;
         document.getElementById("tipo-biglietto").innerHTML = "Normale";
     }
+
+    const carrozza = Math.floor(Math.random() * 10) + 1;
+    console.log(carrozza.value); 
+    document.getElementById("carrozza").innerHTML = carrozza;
+    const codicecp = Math.floor(Math.random() * 99999) + 1;
+    console.log(codicecp.value); 
+    document.getElementById("codicecp").innerHTML = codicecp;
+    
+    document.getElementById("prezzofinale").innerHTML = finalprice.toFixed(2) + " Euro";
 })

0da89809f0880940dec9b5bcd79ba0e8580c06ad
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-02-24 15:39:27 +0100

    js refactoring

--- js/main.js
+++ js/main.js
@@ -1,41 +1,35 @@
-let username;
-let km;
-let eta;
-let basekm = 0.27;
-let price;
+let km = document.getElementById('km');
+let age = document.querySelector('#eta');
+let nameu = document.getElementById('namei');
+const baseprice = 0.27;
+const GeneratorButton = document.getElementById('btninput');
+const output = document.querySelector('#nome-pass');
 
-document.querySelector("#btninput").addEventListener('click', function() {
+GeneratorButton.addEventListener('click', function(){
+
     document.getElementById("risultato").style.display = 'block';
-    username = document.getElementById("namei");
-    console.log(username.value);
-    document.querySelector("#nome-pass").innerHTML = username.value;
 
-    
-    km = document.getElementById("km");
-    console.log(km.value);
-    if( km.value < 1 ){
-        console.warn("I'm sorry, the numer you entered is to small, please reload the page to try again");
-    }
-    price = km * basekm;
-    console.log(price.value);
+    console.log(nameu.value);
+    let ticketPrice = baseprice * km.value;
+    console.log(ticketPrice);
+    console.log(age.value);
 
+    if (age.value == "minorenne"){
+        ticketPrice = ticketPrice * 0.83;
+        console.log('Scontato del 17%');
+        document.getElementById("tipo-biglietto").innerHTML = "Minore";
 
-    eta = document.getElementById("eta");
-    console.log(eta.value);  
-    if(eta = "minorenne"){
-        discount = (price / 100) * 17;
-        finalprice = price - discount;
-        document.getElementById("tipo-biglietto").innerHTML = "Minori";
-    }
-    else if(eta = "over65"){
-        discount = (price / 100) * 33;
-        finalprice = price - discount;
+    } else if ( age.value == "over65" ){
+        ticketPrice -= ticketPrice * 0.77;
+        console.log("Scontato del 33%");
         document.getElementById("tipo-biglietto").innerHTML = "Over 65";
-    }
-    else{
-        finalprice = price;
+
+    } else {
+        console.log("Nessuno sconto applicato");
         document.getElementById("tipo-biglietto").innerHTML = "Normale";
     }
+    console.log(ticketPrice);
+    output.innerHTML = nameu.value;
 
     const carrozza = Math.floor(Math.random() * 10) + 1;
     console.log(carrozza.value); 
@@ -43,6 +37,19 @@ document.querySelector("#btninput").addEventListener('click', function() {
     const codicecp = Math.floor(Math.random() * 99999) + 1;
     console.log(codicecp.value); 
     document.getElementById("codicecp").innerHTML = codicecp;
-    
-    document.getElementById("prezzofinale").innerHTML = finalprice.toFixed(2) + " Euro";
+
+    document.getElementById("prezzofinale").innerHTML = ticketPrice.toFixed(2) + " Euro";
+
 })
+
+const ticketReset = document.getElementById('btnreset');
+
+ticketReset.addEventListener('click', function(){
+
+    nameu.value = "";
+    age.value = "maggiorenne";
+    km.value = "";
+    output.innerHTML = "";
+
+    document.getElementById("risultato").classList.add('d-none');
+});
